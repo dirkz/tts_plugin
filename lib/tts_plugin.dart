9f2bce54c173eca7430e285a146a53995f370c14
Dirk Zimmermann <me@dirkz.com>
2022-06-25 07:10:36 +0200

    Define Voice

--- lib/tts_plugin.dart
+++ lib/tts_plugin.dart
@@ -1,5 +1,11 @@
-
 import 'tts_plugin_platform_interface.dart';
+
+class Voice {
+  String name;
+  String language;
+
+  Voice({required this.name, required this.language});
+}
 
 class TtsPlugin {
   Future<String?> getPlatformVersion() {
