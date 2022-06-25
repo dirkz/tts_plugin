901ca89cd9a2b39e7131f04f54f7f11aceccad71
Dirk Zimmermann <me@dirkz.com>
2022-06-25 07:33:18 +0200

    MethodChannel getVoices

--- lib/tts_plugin_method_channel.dart
+++ lib/tts_plugin_method_channel.dart
@@ -11,7 +11,15 @@ class MethodChannelTtsPlugin extends TtsPluginPlatform {
 
   @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
     return version;
+  }
+
+  @override
+  Future<List<Voice>> getVoices() async {
+    final voices =
+        await methodChannel.invokeMethod<List<Voice>>('getVoices') ?? [];
+    return voices;
   }
 }
