4d82ccb9b8dd05eca09eac83252e5373a18aa643
Dirk Zimmermann <me@dirkz.com>
2022-06-25 17:08:22 +0200

    Trying to implement the html version

--- lib/tts_plugin_web.dart
+++ lib/tts_plugin_web.dart
@@ -2,7 +2,7 @@
 // of your plugin as a separate package, instead of inlining it in the same
 // package as the core of your plugin.
 // ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html show window;
+import 'dart:html' as html show window, SpeechSynthesis;
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -21,6 +21,40 @@ class TtsPluginWeb extends TtsPluginPlatform {
   @override
   Future<String?> getPlatformVersion() async {
     final version = html.window.navigator.userAgent;
-    return version;
+    print('*** version $version');
+    return Future.value(version);
+  }
+
+  @override
+  Future<List<Voice>> getVoices() {
+    final htmlVoices = html.window.speechSynthesis?.getVoices() ?? [];
+    final voices = <Voice>[];
+    for (var htmlVoice in htmlVoices) {
+      final language = htmlVoice.lang;
+      final url = htmlVoice.voiceUri;
+      final name = htmlVoice.name;
+      if (language != null && url != null && name != null) {
+        final voice = Voice(language: language, voiceURL: url, name: name);
+        voices.add(voice);
+      } else {
+        print('*** strange voice $htmlVoice');
+      }
+    }
+    return Future.value(voices);
+  }
+
+  @override
+  Future<bool> setVoice(Voice voice) {
+    return Future.value(false);
+  }
+
+  @override
+  Future<bool> speak(String text) {
+    return Future.value(false);
+  }
+
+  @override
+  Future<bool> cancel() {
+    return Future.value(false);
   }
 }
